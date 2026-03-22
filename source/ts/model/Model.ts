7d1eae2a5b00183dbed0709689a7b3046c4df541
Amrit Agarwal <amagarwal@ingenuitygaming.com>
2026-03-22 13:21:54 +0530

    [MOD] canvas height logic

--- source/ts/model/Model.ts
+++ source/ts/model/Model.ts
@@ -1,8 +1,15 @@
 export class Model {
   private canvasDimensions: { w: number, h: number } = {
     w: 1280,
-    h: innerHeight - (document.getElementById("footer")?.clientHeight ?? 0)
+    h: this.getCanvasHeight()
   };
+
+  private getCanvasHeight(): number {
+    const footerHeight = document.getElementById("footer")?.clientHeight ?? 0;
+    const availableHeight = window.innerHeight - footerHeight;
+
+    return Math.min(720, availableHeight);
+  }
 
   public setCanvasDimensions(w: number, h: number): void {
     this.canvasDimensions = { w, h };
